8aca91be0754fe614b6542f487d1ad7cfa58680d
Qadeer Ullah <qadeeru89@gmail.com>
2025-04-27 00:58:26 +0500

    Apna message yahan likho

---
+++ New.js
@@ -0,0 +1,29 @@
+// Task No 1:
+var teamMembers = [];
+teamMembers = ["Alice", "Bob", "Charlie"];
+console.log(teamMembers);
+
+// Task No 2:
+
+teamMembers.push("Diana","Ethan");
+console.log(teamMembers);
+
+
+// Task No 3:
+teamMembers.pop();
+console.log(teamMembers);
+
+
+// Task No 4:
+teamMembers.unshift("Sophia");
+console.log(teamMembers);
+
+
+// Task No 5:
+teamMembers.shift();
+console.log(teamMembers);
+
+
+
+
+
